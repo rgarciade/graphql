27410ff07ade12211b7ef1db1790f8bc7fb0da59
rgarciade <garcia.de.la.fuente.raul@gmail.com>
2020-07-24 23:49:12 +0200

    buscar estudiante

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -4,6 +4,18 @@ const query : IResolvers = {
     Query : {
         estudiantes(): any {
             return database.estudiantes
+        },
+        estudiante(__: void, {id}): any {
+            const resultado =  database.estudiantes.find(element => element.id === id)
+            return (resultado !== undefined)
+                ? resultado 
+                :   {
+                        id: '-1',
+                        name: `no se ha encontrado el estudiante con el ide ${id}`,
+                        email:'',
+                        courses:[]
+                    } 
+
         }
     }
 }
