a3b597d579c864d06c8eb17b574861ca37649bf4
rgarciade <garcia.de.la.fuente.raul@gmail.com>
2020-07-25 00:23:01 +0200

    more types

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -8,15 +8,36 @@ const query : IResolvers = {
         estudiante(__: void, {id}): any {
             const resultado =  database.estudiantes.find(element => element.id === id)
             return (resultado !== undefined)
-                ? resultado 
-                :   {
-                        id: '-1',
-                        name: `no se ha encontrado el estudiante con el ide ${id}`,
-                        email:'',
-                        courses:[]
-                    } 
-
-        }
+            ? resultado 
+            :   {
+                id: '-1',
+                name: `no se ha encontrado el estudiante con el ide ${id}`,
+                email:'',
+                courses:[]
+            } 
+            
+        },
+        cursos(): any {
+            return database.cursos
+        },
+        curso(__: void, {id}): any {
+            const resultado =  database.cursos.find(element => element.id === id)
+            return (resultado !== undefined)
+            ? resultado 
+            :   {
+                id: -1,
+                title: `no se ha encontrado el curso con el ide ${id}`,
+                description: '',
+                clases: -1,
+                time: 0,
+                logo: '',
+                level: 0,
+                path: '',
+                teacher: '',
+                reviews: []!
+            } 
+            
+        },
     }
 }
 export default query
